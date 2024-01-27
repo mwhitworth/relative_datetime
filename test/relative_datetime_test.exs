13239f08eedc9cd4c042b40d45b9ef602656b107
Matt Whitworth <mwhitworth@gmail.com>
2024-01-27 19:58:38 +0000

    use import

--- test/relative_datetime_test.exs
+++ test/relative_datetime_test.exs
@@ -1,28 +1,30 @@
 defmodule RelativeDateTimeTest do
   use ExUnit.Case, async: true
 
+  import RelativeDateTime, only: [parse: 2]
+
   @now ~U[2018-01-01 00:00:00Z]
 
   test "parses relative dates without given datetime" do
-    assert {:ok, ~U[2018-01-01 00:00:00Z]} = RelativeDateTime.parse("now", @now)
-    assert {:ok, ~U[2017-11-20 00:00:00Z]} = RelativeDateTime.parse("6 weeks ago", @now)
-    assert {:ok, ~U[2018-02-12 00:00:00Z]} = RelativeDateTime.parse("6 weeks", @now)
-    assert {:ok, ~U[2017-12-31 23:59:54.000000Z]} = RelativeDateTime.parse("6 seconds ago", @now)
-    assert {:ok, ~U[2017-12-31 23:54:00.000000Z]} = RelativeDateTime.parse("6 minutes ago", @now)
-    assert {:ok, ~U[2017-12-31 18:00:00.000000Z]} = RelativeDateTime.parse("6 hours ago", @now)
-    assert {:ok, ~U[2017-12-26 00:00:00Z]} = RelativeDateTime.parse("6 days ago", @now)
-    assert {:ok, ~U[2017-07-01 00:00:00Z]} = RelativeDateTime.parse("6 months ago", @now)
-    assert {:ok, ~U[2012-01-01 00:00:00Z]} = RelativeDateTime.parse("6 years ago", @now)
+    assert {:ok, ~U[2018-01-01 00:00:00Z]} = parse("now", @now)
+    assert {:ok, ~U[2017-11-20 00:00:00Z]} = parse("6 weeks ago", @now)
+    assert {:ok, ~U[2018-02-12 00:00:00Z]} = parse("6 weeks", @now)
+    assert {:ok, ~U[2017-12-31 23:59:54.000000Z]} = parse("6 seconds ago", @now)
+    assert {:ok, ~U[2017-12-31 23:54:00.000000Z]} = parse("6 minutes ago", @now)
+    assert {:ok, ~U[2017-12-31 18:00:00.000000Z]} = parse("6 hours ago", @now)
+    assert {:ok, ~U[2017-12-26 00:00:00Z]} = parse("6 days ago", @now)
+    assert {:ok, ~U[2017-07-01 00:00:00Z]} = parse("6 months ago", @now)
+    assert {:ok, ~U[2012-01-01 00:00:00Z]} = parse("6 years ago", @now)
   end
 
   test "returns error for unparsable datetimes" do
-    assert :error = RelativeDateTime.parse("nonsense", @now)
-    assert :error = RelativeDateTime.parse("sox weeks ago", @now)
-    assert :error = RelativeDateTime.parse("2018-13-01", @now)
+    assert :error = parse("nonsense", @now)
+    assert :error = parse("sox weeks ago", @now)
+    assert :error = parse("2018-13-01", @now)
   end
 
   test "parses given datetimes" do
-    assert {:ok, ~U[2018-01-01 00:00:00Z]} = RelativeDateTime.parse("2018-01-01", @now)
-    assert {:ok, ~U[2018-06-01 12:34:56Z]} = RelativeDateTime.parse("2018-06-01 12:34:56", @now)
+    assert {:ok, ~U[2018-01-01 00:00:00Z]} = parse("2018-01-01", @now)
+    assert {:ok, ~U[2018-06-01 12:34:56Z]} = parse("2018-06-01 12:34:56", @now)
   end
 end
